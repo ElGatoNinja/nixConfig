223a6ecc1eefb8f02bbbdc43e0dc1a4a285c2f00
ElGatoNinja <JaimeGAlonso@outlook.com>
2024-01-25 21:23:56 +0100

    intital hyprland config

---
+++ modules/hyprland/default.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:{
+  home.packages = with pkgs; [
+      wofi 
+      swaybg 
+      wlsunset 
+      wl-clipboard 
+      hyprland
+  ];
+  home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
+}
