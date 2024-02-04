7be971a91b1d279c73c975d2e8b24da965e49553
ElGatoNinja <JaimeGAlonso@outlook.com>
2024-02-04 12:29:17 +0100

    i18n

--- hosts/home/configuration.nix
+++ hosts/home/configuration.nix
@@ -10,6 +10,7 @@
       ./hardware-configuration.nix
       ../../modules/system/garbageCollection.nix
       ../../modules/system/hyprland.nix
+      ../../modules/system/i18n.nix
 
       ../../modules/hardware/bluetooth.nix
     ];
@@ -29,23 +30,7 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Madrid";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "es_ES.UTF-8";
-    LC_IDENTIFICATION = "es_ES.UTF-8";
-    LC_MEASUREMENT = "es_ES.UTF-8";
-    LC_MONETARY = "es_ES.UTF-8";
-    LC_NAME = "es_ES.UTF-8";
-    LC_NUMERIC = "es_ES.UTF-8";
-    LC_PAPER = "es_ES.UTF-8";
-    LC_TELEPHONE = "es_ES.UTF-8";
-    LC_TIME = "es_ES.UTF-8";
-  };
+  
 
   # Enable the X11 windowing system.
   services.xserver = {
